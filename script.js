c03ad1b448795b77846e22550b0dd6f0e66479a8
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-03-23 18:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+sayHello().then((message) => {
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = message;
+});
